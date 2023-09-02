03d27696ad8c9183844b50577e04fea071a11871
zilla <106297133+zillastar@users.noreply.github.com>
2023-09-02 15:31:55 +0800

    Set-up base structure for login & register pages. #5

---
+++ frontend/src/screens/LoginScreen.js
@@ -0,0 +1,34 @@
+import { useRef } from "react"
+
+const LoginScreen = () => {
+
+    // useRef to pull login data (username and password)
+    const userName = useRef(null);
+    const password = useRef(null);
+
+    return (
+        <div className="container">
+            <div className="text-center interFont" style={{ marginTop: '15rem' }}>
+                <h1 className="interFont2">Login</h1>
+                <div className="row">
+                    <div className="col-2"></div>
+                    <div className="col-8">
+                        <form style={{ marginTop: '2rem' }}>
+                            <div className="form-group">
+                                <input type="text" className="form-control p-3" id="username" ref={userName} rows="3" placeholder="Username" />
+                                <input type="password" className="form-control p-3" id="password" rows="3" ref={password} placeholder="Password" />
+                            </div>
+                            <button type="button" className="btn btn-primary btn-lg interFont2 p-3 px-4" style={{ marginTop: '2rem' }}>Login</button>
+                            <p className="mt-2">
+                                <a className="loginOrRegisterHref" href="/register">Don't have an account? Register</a>
+                            </p>
+                        </form>
+                    </div>
+                    <div className="col-2"></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default LoginScreen
