fb5295b99db7909f30883a71eecfd3487638cfc2
zilla <106297133+zillastar@users.noreply.github.com>
2023-09-03 23:46:22 +0800

    adding alt

--- frontend/src/screens/ConfirmedScreen.js
+++ frontend/src/screens/ConfirmedScreen.js
@@ -6,7 +6,7 @@ const ConfirmedScreen = () => {
             <NavBar />
             <div className="container">
                 <div className="d-flex flex-column justify-content-center align-items-center" id="formSubmit" >
-                    <img src={require("../assets/tick.png")} style={{ width: "200px" }} />
+                    <img src={require("../assets/tick.png")} style={{ width: "200px" }} alt="tick" />
                     <div className="text-center mt-3 interFont">
                         <h4>Form successfully submitted!</h4>
                     </div>
