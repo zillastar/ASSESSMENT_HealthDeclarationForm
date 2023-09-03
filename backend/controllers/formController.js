cf1c82bb660683b8020c49e5c562e68cfc3a8882
zilla <106297133+zillastar@users.noreply.github.com>
2023-09-03 23:13:42 +0800

    Added 3 new endpoints for Forms backend. #8

--- backend/controllers/formController.js
+++ backend/controllers/formController.js
@@ -31,7 +31,25 @@ module.exports.getFormByUser = function (req, res) {
         .catch((error) => {
             res.send(error);
         });
-}
+};
+
+module.exports.getFormByUQID = function (req, res) {
+    let id = req.query.id
+
+    const sqlStatement = `SELECT * from formUnique WHERE formUQID = ?;`;
+    const values = [id]
+
+    connection
+        .query(sqlStatement, values)
+        .then(([rows, fields]) => {
+            console.log(rows);
+            res.json(rows);
+        })
+        .catch((error) => {
+            res.send(error);
+        });
+};
+
 
 module.exports.createNewForm = function (req, res) {
     let form = req.body
@@ -84,6 +102,63 @@ module.exports.postForm = function (req, res) {
         .then(([rows, fields]) => {
             console.log(rows);
             res.json(rows);
+        })
+        .catch((error) => {
+            res.send(error);
+        });
+}
+
+module.exports.updateForm = function (req, res) {
+    let form = req.body;
+
+    // checking userID also for better validation
+    const sqlStatement = `UPDATE formUnique SET name = COALESCE(?, name), description = COALESCE(?, name) WHERE formUQID = ? AND userID = ?;`;
+    const values = [
+        form.name,
+        form.description,
+        form.formUQID,
+        form.userID
+    ];
+
+    connection
+        .query(sqlStatement, values)
+        .then(([rows, fields]) => {
+            console.log(rows);
+            res.json(rows);
+        })
+        .catch((error) => {
+            res.send(error);
+        });
+};
+
+module.exports.deleteForm = function (req, res) {
+    let form = req.query;
+
+    // checking userID also for better validation
+    const sqlStatement = `DELETE FROM formUnique WHERE formUQID = ? AND userID = ?;`;
+    const values = [
+        form.formUQID,
+        form.userID
+    ];
+
+    const sqlStatement2 = `DELETE FROM form WHERE formUQID = ?;`;
+    const values2 = [
+        form.formUQID
+    ];
+
+    connection
+        .query(sqlStatement, values)
+        .then(([rows, fields]) => {
+            console.log(rows);
+            connection
+                .query(sqlStatement2, values2)
+                .then(([rows, fields]) => {
+                    console.log(rows);
+                    res.json(rows);
+                })
+                .catch((error) => {
+                    res.send(error);
+                });
         })
         .catch((error) => {
             res.send(error);
--- backend/routes/formRoutes.js
+++ backend/routes/formRoutes.js
@@ -4,8 +4,11 @@ const router = express.Router();
 const controller = require("../controllers/formController");
 router.get("/", controller.getForm);
 router.get("/byUser", controller.getFormByUser);
+router.get("/byUQID", controller.getFormByUQID);
 router.get("/data", controller.getFormData);
 router.post("/newform", controller.createNewForm);
 router.post("/", controller.postForm);
+router.put("/", controller.updateForm);
+router.delete("/", controller.deleteForm);
 
 module.exports = router;
